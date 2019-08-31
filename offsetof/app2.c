15beeab78b3014e7e20f0397bc545bb75e294ce9
Cqlismy <157823712@qq.com>
2019-08-31 18:38:11 +0800

    add new files

---
+++ offsetof/app2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#pragma pack(2)
+struct s {
+	char a;
+	short b;
+	char c;
+	int d;
+	char e[3];
+};
+#pragma pack()
+
+int main(int argc, char *argv[])
+{
+	printf("sizeof(struct s):%zd\n", sizeof(struct s));
+	printf("offsets->a:%zd; b:%zd; c:%zd; d:%zd; e[0]:%zd; e[1]:%zd; e[2]:%zd\n",
+					offsetof(struct s, a), offsetof(struct s, b), 
+					offsetof(struct s, c), offsetof(struct s, d),
+					offsetof(struct s, e[0]), offsetof(struct s, e[1]),
+					offsetof(struct s, e[2]));
+
+	exit(EXIT_SUCCESS);
+}
+
+
